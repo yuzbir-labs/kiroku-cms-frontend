997e0c63526b23f916cd92e2e869a309e8c3c990
NihadMammadli <nihadmammadli03@gmail.com>
2025-11-05 18:21:31 +0400

    feat(config): :sparkles: update API configuration to use environment variable and add @types/node dependency
    - Modified the base URL in the API configuration to utilize the VITE_API_URL environment variable, enhancing flexibility for different environments.
    - Added @types/node to devDependencies for improved TypeScript support.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8000,
-    proxy: {
-      '/api': {
-        target: 'http://10.201.10.155:8000',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 8000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:8000',
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
